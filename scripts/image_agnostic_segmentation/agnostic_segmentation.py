f282751520f03a059b9875081868d3c1622908bf
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-02-07 16:19:09 +0100

    change feature matching to SIFT instead of ORB

--- scripts/image_agnostic_segmentation/agnostic_segmentation.py
+++ scripts/image_agnostic_segmentation/agnostic_segmentation.py
@@ -53,16 +53,31 @@ def find_object_mask(img_original, object_images_path, predictions):
 
     features_all = list()
 
-    orb = cv2.ORB_create()
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    sift = cv2.SIFT_create()
+    #bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    bf = cv2.BFMatcher()
 
     instances = predictions["instances"].to("cpu")
+
+    # Visualize masks
+    for i in range(len(instances)):
+        # TODO should bounding boxex be used instead of masks
+        # mask original image with predicted mask
+        instance = instances[i]
+        # masks = np.array([instances.pred_masks[i].cpu().detach().numpy() for i in range(len(instance))])
+        mask = instance.pred_masks.cpu().detach().numpy()[0]
+        # pred_masks = instance.pred_masks[index].cpu().detach().numpy()
+        masked_img = img_original.copy()
+        masked_img[mask == False] = np.array([0, 0, 0])
+
+        cv2.imshow('Masked_image', masked_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     img = img_original.copy()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    kp1 = orb.detect(img, None)
-    kp1, des1 = orb.compute(img, kp1)
+    kp1, des1 = sift.detectAndCompute(img, None)
 
     for image_path in object_images_paths:
         object_img = cv2.imread(image_path)
@@ -71,26 +86,31 @@ def find_object_mask(img_original, object_images_path, predictions):
         features_per_face = list()
 
         # find the keypoints and descriptors with ORB
-        kp2 = orb.detect(object_img, None)
-        kp2, des2 = orb.compute(object_img, kp2)
+        kp2, des2 = sift.detectAndCompute(object_img, None)
 
         # Match descriptors.
-        matches = bf.match(des1, des2)
+        matches = bf.knnMatch(des1, des2, k=2)
 
         # Sort them in the order of their distance.
-        matches = sorted(matches, key=lambda x: x.distance)
+        #matches = sorted(matches, key=lambda x: x.distance)
+
+        # Apply ratio test
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good_matches.append([m])
 
         # Draw first 40 matches.
         img_matches = np.empty((max(img.shape[0], object_img.shape[0]), img.shape[1] + object_img.shape[1], 3),
                                dtype=np.uint8)
-        img3 = cv2.drawMatches(img, kp1, object_img, kp2, matches[:40], img_matches,
-                               flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        #img3 = cv2.drawMatches(img, kp1, object_img, kp2, matches, img_matches, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img3 = cv2.drawMatchesKnn(img, kp1, object_img, kp2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
         cv2.imshow('Good Matches', img3)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        list_kp1 = [kp1[mat.queryIdx].pt for mat in matches]
+        list_kp1 = [kp1[mat[0].queryIdx].pt for mat in good_matches]
         list_kp1 = [(int(element[0]), int(element[1])) for element in list_kp1]
 
         for i in range(len(instances)):
@@ -111,6 +131,13 @@ def find_object_mask(img_original, object_images_path, predictions):
     features_all = list(map(list, zip(*features_all)))
     print(features_all)
 
+    # normalize number of features
+    sum = np.sum(np.array(features_all).flatten())
+    features_all = [[float(j)/sum for j in i] for i in features_all]
+    print(features_all)
+
+    # TODO use a Threshold for multi mask detection instead of max
+    print([np.sum(x) for x in features_all])
     ind = np.argmax([np.sum(x) for x in features_all])
 
     return predictions[ind]
