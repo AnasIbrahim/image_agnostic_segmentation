40e957b00f86e52fd0e8e86dc8df4b41971fd0fe
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-02-07 03:50:16 +0100

    clean non ROS test script

--- scripts/image_agnostic_segmentation/agnostic_segmentation.py
+++ scripts/image_agnostic_segmentation/agnostic_segmentation.py
@@ -1,30 +1,25 @@
-import cv2
-import time
+import numpy as np
+
 from detectron2.utils.logger import setup_logger
 from detectron2 import model_zoo
-from detectron2.engine import DefaultTrainer, DefaultPredictor, launch
+from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.structures import BoxMode
 from detectron2.utils.visualizer import ColorMode
 
-import rospkg
 
 setup_logger()  # initialize the detectron2 logger and set its verbosity level to “DEBUG”.
 
-def segment_image(im_input_input, im_input_visualize, model_path):
+
+def segment_image(img, model_path):
     confidence = 0.95
 
     # --- var dec ---
-    MetadataCatalog.get("kitchen0_val").set(thing_classes=["object"])
-    kitchen0_metadata = MetadataCatalog.get("kitchen0_val")
 
     # --- Config setup ---
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))  # WAS 3x.y
-    rospack = rospkg.RosPack()
-    rospack.list()
     cfg.MODEL.WEIGHTS = model_path
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
     cfg.MODEL.SEM_SEG_HEAD.NUM_CLASSES = 1
@@ -34,16 +29,20 @@ def segment_image(im_input_input, im_input_visualize, model_path):
     cfg.MODEL.DEVICE = 'cpu'
     predictor = DefaultPredictor(cfg)
 
-    start = time.time()
-    outputs = predictor(im_input_input)
-    end = time.time()
-    print("Inference time: ", end - start)
-    v = Visualizer(im_input_visualize,
+    predictions = predictor(img)
+
+    return predictions
+
+
+def visualize_segmented_img(img, predictions):
+    MetadataCatalog.get("user_data").set(thing_classes=["object"])
+    kitchen0_metadata = MetadataCatalog.get("kitchen0_val")
+    v = Visualizer(img,
                    metadata=kitchen0_metadata,
                    scale=0.5,
                    instance_mode=ColorMode.IMAGE
                    )
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    out = v.draw_instance_predictions(predictions["instances"].to("cpu"))
     img = out.get_image()
-
+    
     return img
