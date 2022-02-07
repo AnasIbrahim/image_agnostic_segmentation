646534df56b927db42b05f427d6d43a08b615216
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-02-07 04:12:16 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 curl -J -O "https://tu-dortmund.sciebo.de/s/qfNQ2vLdCXW8RBS/FAT_trained_Ml2R_bin_fine_tuned.pth"
 
+python3 scripts/image_agnostic_segmentation/segment_image.py --image-path IMAGE_PATH --model-path MODEL_PATH
--- scripts/image_agnostic_segmentation/segment_image.py
+++ scripts/image_agnostic_segmentation/segment_image.py
@@ -7,15 +7,15 @@ import agnostic_segmentation
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--img", type=str, help="path to image", default="")
-    parser.add_argument("--model_path", type=str, help="path to class-agnostic model", default='')
+    parser.add_argument("--image-path", type=str, help="path to image", required=True)
+    parser.add_argument("--model-path", type=str, help="path to class-agnostic model", required=True)
     args = parser.parse_args()
 
     img = cv2.imread(args.image_path)
     predictions = agnostic_segmentation.segment_image(img, args.model_path)
     seg_img = agnostic_segmentation.visualize_segmented_img(img, predictions)
 
-    cv2.imshow('Image', seg_img)
+    cv2.imshow('segmented_image', seg_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
