3831ff5ec501f27068c509f0885d1c39b54c99f4
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-02-07 05:33:47 +0100

    add object feature matching

--- scripts/image_agnostic_segmentation/agnostic_segmentation.py
+++ scripts/image_agnostic_segmentation/agnostic_segmentation.py
@@ -1,4 +1,7 @@
+import glob
+
 import numpy as np
+import cv2
 
 from detectron2.utils.logger import setup_logger
 from detectron2 import model_zoo
@@ -13,11 +16,9 @@ setup_logger()  # initialize the detectron2 logger and set its verbosity level t
 
 
 def segment_image(img, model_path):
-    confidence = 0.95
+    confidence = 0.9
 
-    # --- var dec ---
-
-    # --- Config setup ---
+    # --- detectron2 Config setup ---
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))  # WAS 3x.y
     cfg.MODEL.WEIGHTS = model_path
@@ -34,15 +35,75 @@ def segment_image(img, model_path):
     return predictions
 
 
-def visualize_segmented_img(img, predictions):
+def draw_segmented_image(img, predictions):
     MetadataCatalog.get("user_data").set(thing_classes=["object"])
-    kitchen0_metadata = MetadataCatalog.get("kitchen0_val")
+    metadata = MetadataCatalog.get("user_data")
     v = Visualizer(img,
-                   metadata=kitchen0_metadata,
+                   metadata=metadata,
                    scale=0.5,
                    instance_mode=ColorMode.IMAGE
                    )
     out = v.draw_instance_predictions(predictions["instances"].to("cpu"))
     img = out.get_image()
     
+    return img
+
+def find_object_mask(img, object_images_path, predictions):
+    object_images_paths = glob.glob(object_images_path+'/*')
+
+    orb = cv2.ORB_create()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    instances = predictions["instances"].to("cpu")
+    for i in range(len(instances)):
+        # TODO should bounding boxex be used instead of masks
+        # mask original image with predicted mask
+        instance = instances[i]
+        #masks = np.array([instances.pred_masks[i].cpu().detach().numpy() for i in range(len(instance))])
+        mask = instance.pred_masks.cpu().detach().numpy()[0]
+        #pred_masks = instance.pred_masks[index].cpu().detach().numpy()
+        masked_img = img.copy()
+        masked_img[mask == False] = np.array([0, 0, 0])
+
+        cv2.imshow('Masked_image', masked_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        kp1 = orb.detect(masked_img, None)
+        kp1, des1 = orb.compute(img, kp1)
+
+        for image_path in object_images_paths:
+            object_img = cv2.imread(image_path)
+
+            # find the keypoints and descriptors with ORB
+            kp2 = orb.detect(object_img, None)
+            kp2, des2 = orb.compute(object_img, kp2)
+
+            # Match descriptors.
+            matches = bf.match(des1, des2)
+
+            # Sort them in the order of their distance.
+            matches = sorted(matches, key=lambda x: x.distance)
+
+            # Draw first 10 matches.
+            #img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10])
+            img_matches = np.empty((max(img.shape[0], object_img.shape[0]), img.shape[1] + object_img.shape[1], 3), dtype=np.uint8)
+            img3 = cv2.drawMatches(masked_img, kp1, object_img, kp2, matches[:40], img_matches,
+                           flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+            cv2.imshow('Good Matches', img3)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+def draw_found_masks(img, object_predictions, object):
+    MetadataCatalog.get("user_data").set(thing_classes=[object])
+    metadata = MetadataCatalog.get("user_data")
+    v = Visualizer(img,
+                   metadata=metadata,
+                   scale=0.5,
+                   instance_mode=ColorMode.IMAGE
+                   )
+    out = v.draw_instance_predictions(object_predictions["instances"].to("cpu"))
+    img = out.get_image()
+
     return img
--- scripts/image_agnostic_segmentation/segment_image.py
+++ scripts/image_agnostic_segmentation/segment_image.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os
+
 import cv2
 import argparse
 
@@ -9,15 +11,27 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--image-path", type=str, help="path to image", required=True)
     parser.add_argument("--model-path", type=str, help="path to class-agnostic model", required=True)
+    parser.add_argument("--objects-images", type=str, help="path to object images to be segmented \
+                                                            ex: objects_folder/[book,shampoo,...]")
     args = parser.parse_args()
 
     img = cv2.imread(args.image_path)
     predictions = agnostic_segmentation.segment_image(img, args.model_path)
-    seg_img = agnostic_segmentation.visualize_segmented_img(img, predictions)
+    seg_img = agnostic_segmentation.draw_segmented_image(img, predictions)
 
     cv2.imshow('segmented_image', seg_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    if args.objects_images_folder is not None:
+        objects = os.listdir(args.objects_images_folder)
+        for object in objects:
+            object_predictions = agnostic_segmentation.find_object_mask(img, os.path.join(args.objects_images_folder,object), predictions)
+            agnostic_segmentation.draw_segmented_image(args.img, object_predictions)
+
+            cv2.imshow(object + '  masks', seg_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
