b6fabafd0d20c11c711232b51e3aa031f35dd95a
Anas Gouda <anas.gouda@tu-dortmund.de>
2022-02-18 02:13:39 +0100

    add visualization to suction grasp vector

--- scripts/image_agnostic_segmentation/compute_grasp.py
+++ scripts/image_agnostic_segmentation/compute_grasp.py
@@ -50,8 +50,9 @@ def compute_suction_points(rgb_img, depth_img, c_matrix, predictions):
         #plane_cloud.paint_uniform_color([1.0, 0, 0])
 
 
-        plane_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-        plane_cloud.orient_normals_towards_camera_location(camera_location=np.array([0., 0., 0.]))
+        plane_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=100))
+        #plane_cloud.orient_normals_towards_camera_location(camera_location=np.array([0., 0., 0.]))
+        plane_cloud.orient_normals_towards_camera_location(np.array([0,0,-1]))
 
         #o = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
         #o3d.visualization.draw_geometries([o, plane_cloud])
@@ -59,18 +60,24 @@ def compute_suction_points(rgb_img, depth_img, c_matrix, predictions):
         grasp_position = plane_cloud.get_center()
         pcd_tree = o3d.geometry.KDTreeFlann(plane_cloud)
         [k, idx, _] = pcd_tree.search_knn_vector_3d(grasp_position, 1)
-        grasp_orientation = pose_from_vector3D(np.asarray(plane_cloud.normals)[idx[0], :])
+
+        point = np.asarray(plane_cloud.points)[idx[0], :]
+        normal = np.asarray(plane_cloud.normals)[idx[0], :]
+        grasp_orientation = pose_from_vector3D(normal-point)
 
         # visualize to check grasp computation
-        grasp_arrow = o3d.geometry.TriangleMesh.create_arrow(cylinder_radius=0.01, cone_radius=0.015, cylinder_height=0.05, cone_height=0.02)
-        grasp_arrow.paint_uniform_color(np.array([255,0,0]))
-        grasp_arrow.translate(grasp_position)
+        grasp_arrow = o3d.geometry.TriangleMesh.create_arrow(cylinder_radius=0.005, cone_radius=0.0075, cylinder_height=0.05, cone_height=0.02)
+        grasp_arrow.paint_uniform_color(np.array([1,0,0]))
+        grasp_arrow.rotate(o3d.geometry.get_rotation_matrix_from_xyz((0,np.pi/2,0)))
+        grasp_arrow.translate((0,0, -0.03))
         rot_mat = o3d.geometry.get_rotation_matrix_from_quaternion(grasp_orientation)
-        #grasp_arrow.rotate(rot_mat)
+        grasp_arrow.rotate(rot_mat)
+        grasp_arrow.translate(grasp_position)
+        grasp_arrow.translate((0,0, -0.03))
         o = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
-        o3d.visualization.draw_geometries([o, pcd, grasp_arrow])
+        #o3d.visualization.draw_geometries([o, plane_cloud, grasp_arrow])
 
-        suction_pts.append((tuple(grasp_position), grasp_orientation))
+        suction_pts.append((tuple(grasp_position), tuple(grasp_orientation)))
 
     return suction_pts
 
@@ -78,8 +85,34 @@ def compute_suction_points(rgb_img, depth_img, c_matrix, predictions):
 def visualize_suction_points(rgb_img, c_matrix, suction_pts):
     suction_pts_img = copy.deepcopy(rgb_img)
     for point in suction_pts:
+        grasp_position = np.array(point[0])
+        grasp_orientation = np.array(point[1])
+
+        # visualize to check grasp computation
+        grasp_arrow = o3d.geometry.TriangleMesh.create_arrow(cylinder_radius=0.005, cone_radius=0.0075, cylinder_height=0.05, cone_height=0.02)
+        grasp_arrow.paint_uniform_color(np.array([1,0,0]))
+        grasp_arrow.rotate(o3d.geometry.get_rotation_matrix_from_xyz((0,np.pi/2,0))) # align arrow with x axis
+        grasp_arrow.translate((0,0, -0.03))  # make arrow center at origin (half of arrow length)
+        rot_mat = o3d.geometry.get_rotation_matrix_from_quaternion(grasp_orientation)
+        grasp_arrow.rotate(rot_mat)
+        grasp_arrow.translate(grasp_position)
+        grasp_arrow.translate((0,0, -0.03))  # make tip of the arrow the point not its center
+        o = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
+
         # project 3D grasp point on image
+        arrow_cloud = grasp_arrow.sample_points_uniformly(number_of_points=50000)
+        arrow_point = np.asarray(arrow_cloud.points)
+        rvec = np.array([0, 0, 0], np.float)
+        tvec = np.array([0, 0, 0], np.float)
+        impoints, _ = cv2.projectPoints(arrow_point, rvec, tvec, c_matrix, None)
+        impoints = np.squeeze(impoints, axis=1)
+        impoints = impoints.astype(np.int)
+        impoints = (impoints[:,1],impoints[:,0])
+
         # mask rgb with the grasp point
-        pass
+        try:
+            suction_pts_img[impoints] = (0,255,0)
+        except: # if pixel outside of image range (part of the arrow then), do nothing
+            pass
 
     return suction_pts_img
